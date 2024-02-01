e36cccb58e35e11a1118cfc16158783d039b9299
ensag-dev <59049879+ensag-dev@users.noreply.github.com>
2024-02-01 09:08:30 +0100

    Create oc.php

---
+++ plx_artViews/lang/oc.php
@@ -0,0 +1,26 @@
+<?php
+
+$LANG = array(
+'L_VIEWS'						=>' vistas(s)',
+'L_EXCLUDE_BOTS'				=>' exclure los robòts',
+'L_NBARTS_SHOW'					=>' Nombre d’article de mostrar',
+'L_SAVE_TO_UPDATE'				=>' Validar las MAJ ',
+'L_PLUGIN_DEFAULT_SETTING'		=>' Valors predeterminada ',
+'L_RESET_TO_DEFAULT'			=>' Reïnicializar',
+'L_SAVE'						=>' Enregistrar ',
+'L_CODE_TO_INSERT_TO_TPLS'		=>' Còdi a inserir dins vòstres fichièrs template del tèma ',
+'L_CODE_TO_INSERT_COPIED'		=>' Còdi a inserir dins vòstres fichièrs template <b>copiat</b> ',
+'L_VIEWS_INFOS'					=>' Infos collectadas per l’extension dempuèi lo : ',
+'L_TOTAL_VIEWS'					=>' Nombre de vistas globalas ',
+'L_MOST_VIEWED'					=>' Articles mai vistes',
+'L_VIEW_CODE'					=>' Còdis a inserir',
+'L_VIEW_LIST_CODE'				=>' Afichatge lista dels mai legits ',		
+'L_CODE_TO_INSERT_MOST'			=>' Còdi per afichar la lista dels mai legits dins vòstres tèmas ',
+'L_PAGE_404'					=>' Pagina 404 : ',
+
+#dates
+'L_DATE_LANG'						=> array('gen.','febr.',' març','abr.','mai','junh','jul.','ago.','sep.','oct.','nov.','dec.'),
+
+
+);
+?>
